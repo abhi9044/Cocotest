65ad3275a245d8d89a7c5631ad9475bf732f681d
Abhishek Singh <abhishek111204cse@gmail.com>
2021-04-30 16:18:10 +0530

    added networking api

--- Sources/CocoTest/Networking.swift
+++ Sources/CocoTest/Networking.swift
@@ -9,6 +9,23 @@ import Foundation
 
 extension CocoTest{
     public class Networking{
-        
+        public class Manager{
+            public init(){}
+            private let session = URLSession.shared
+            
+            public func loadData(from url:URL, comletionHandler:@escaping
+                                    (NetworkResult<Data>) -> Void){
+                let task = session.dataTask(with: url){data, response, error in
+                    let result  = data.map(NetworkResult<Data>.success) ??
+                        .failure(error)
+                    comletionHandler(result)
+                }
+                task.resume()
+            }
+        }
+        public enum NetworkResult<Value>{
+            case success(Value)
+            case failure(Error?)
+        }
     }
 }
--- Tests/CocoTestTests/CocoNetworkingTest.swift
+++ Tests/CocoTestTests/CocoNetworkingTest.swift
@@ -6,13 +6,31 @@
 //
 
 import XCTest
+@testable import CocoTest
 
 final class CocoNetworkingTest: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+      
+    }
+    func testLoadDataCall(){
+        let manager = CocoTest.Networking.Manager()
+        let expectation = XCTestExpectation(description: "Called for data")
+        guard  let url = URL(string: "https://raywenderlich.com") else {
+           return XCTFail("could not create url properly")
+        }
+        manager.loadData(from:url){ result in
+            expectation.fulfill()
+            switch result{
+            case .success(let returnedData):
+                XCTAssertNotNil(returnedData,"response data is nil")
+                
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "error in result")
+            }
+        }
+        wait(for: [expectation], timeout: 5)
     }
     static var allTest = [
-    ("testExample",testExample)
+    ("testLoadDataCall",testLoadDataCall)
     ]
 }
